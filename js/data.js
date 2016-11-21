cd753a4f81d67867c84d1166e74650f04d6ee679
Christopher Porter <christopher.porter@yardi.com>
2016-11-20 21:06:46 -0800

    Add parsing of sleep data

---
+++ js/data.js
@@ -0,0 +1,40 @@
+class Data {
+	constructor (data) {
+		this.data = data;
+	}
+}
+
+class Excretions extends Data {
+}
+
+class Feeds extends Data {
+}
+
+class Sleeps extends Data {
+	constructor (data, dayNightHour) {
+		super(data);
+		this.dayNightHour = dayNightHour || 8; // daytime 8 am - 8 pm
+		console.assert(this.dayNightHour < 12, 'dayNightHour must be between 0 and 11');
+		_.each(this.data, function (sleep) {
+			sleep["Start Time"] = sleep["Start Time"] && new Date(sleep["Start Time"]);
+			sleep["End Time"] = sleep["End Time"] && new Date(sleep["End Time"]);
+
+			if (sleep["End Time"]) {
+				sleep["Mid Time"] = new Date((sleep["Start Time"].getTime() + sleep["End Time"].getTime()) / 2);
+			} else {
+				sleep["Mid Time"] = sleep["Start Time"];
+			}
+
+			sleep["Nap"] = sleep["Mid Time"].getHours() >= this.dayNightHour && sleep["Mid Time"].getHours() < this.dayNightHour + 12;
+		});
+	}
+
+	get naps() {
+		_.where(this.data, {"Nap": true});
+	}
+
+	get nightSleeps() {
+		_.where(this.data, {"Nap": false});
+	}
+}
+
