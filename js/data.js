9215d69cbe4736e0333ac8d9c414dceb3965c32d
Chris Porter <chris.porter@newhippo.com>
2018-11-12 12:55:47 -0800

    use es6 syntax

--- js/data.js
+++ js/data.js
@@ -5,17 +5,17 @@ class Data {
 		this.rows = [];
 		this.dateFormat = dateFormat;
 
-		_.each(dataAsJson, function (datum) {
+		_.each(dataAsJson, (datum) => {
 
 			// Trim property names in datum
 			var trimDatum = {};
-			_.each(datum, function (value, key) {
+			_.each(datum, (value, key) => {
 				trimDatum[key.trim()] = value;
 			});
 
 			var columnData = [];
 			var cleansedDatum = {};
-			_.each(columns, function (column) {
+			_.each(columns, (column) => {
 				let value;
 				if (column.derivativeFn) {
 					// Pass columnData for previously defined columns and raw row
@@ -51,7 +51,7 @@ class Data {
 	}
 
 	columnIdToIndex(columnId) {
-		return _.findIndex(this.columns, function (column) {
+		return _.findIndex(this.columns, (column) => {
 			return column.id === columnId;
 		})
 	}
@@ -67,7 +67,7 @@ class Data {
 		var view = new google.visualization.DataView(this.dataTable());
 
 		if (columnIds) {
-			view.setColumns(_.map(columnIds, function (column) {
+			view.setColumns(_.map(columnIds, (column) => {
 				if (_.isNumber(column)) {
 					return this.columnIdToIndex(column);
 				} else {
@@ -98,13 +98,13 @@ class Excretions extends Data {
 		super(data, [
 			{id: 'id', label: 'id', type: 'number'},
 			{id: 'time', label: 'Time', type: 'datetime'},
-			{id: 'start', label: 'Start Time', derivativeFn: function (cleansedData, rawRow) {
+			{id: 'start', label: 'Start Time', derivativeFn: (cleansedData, rawRow) => {
 				return cleansedData['time'].clone().subtract(diaperChangeDurationInMinutes, 'minutes');
 			}, type: 'datetime'},
-			{id: 'end', label: 'End Time', derivativeFn: function (cleansedData, rawRow) {
+			{id: 'end', label: 'End Time', derivativeFn: (cleansedData, rawRow) => {
 				return cleansedData['time'].clone();
 			}, type: 'datetime'},
-			{id: 'day', label: 'Day', derivativeFn: function (cleansedData, rawRow) {
+			{id: 'day', label: 'Day', derivativeFn: (cleansedData, rawRow) => {
 				return cleansedData['time'].clone().startOf('day');
 			}, type: 'date'},
 			{id: 'type', label: 'Type', type: 'string'},
@@ -122,9 +122,9 @@ class Excretions extends Data {
 		dataTable.addColumn({id: 'pees', label: 'Pees', type: 'number'});
 		dataTable.addColumn({id: 'poos', label: 'Poos', type: 'number'});
 
-		_.each(this.diapersByDay, function (diapers, day) {
+		_.each(this.diapersByDay, (diapers, day) => {
 			let pees = 0, poos = 0;
-			_.each(diapers, function (diaper) {
+			_.each(diapers, (diaper) => {
 				if (diaper.type === 'Pee') {
 					pees++;
 				} else if (diaper.type === 'Pee and Poo' || diaper.type === 'Poo') {
@@ -143,7 +143,7 @@ class Feeds extends Data {
 		super(data, [
 			{id: 'id', label: 'id', type: 'number'},
 			{id: 'start', label: 'Start Time', type: 'datetime'},
-			{id: 'end', label: 'End Time', derivativeFn: function (cleansedData, rawRow) {
+			{id: 'end', label: 'End Time', derivativeFn: (cleansedData, rawRow) => {
 				let startTime = cleansedData['start'];
 				let endTime = rawRow['End Time'];
 				if (endTime) {
@@ -152,12 +152,12 @@ class Feeds extends Data {
 					return startTime.clone().add(rawRow['Duration (Minutes)'], 'minutes');
 				}
 			}, type: 'datetime'},
-			{id: 'time', label: 'Time', derivativeFn: function (cleansedData, rawRow) {
+			{id: 'time', label: 'Time', derivativeFn: (cleansedData, rawRow) => {
 				let startTime = cleansedData['start'];
 				let endTime = cleansedData['end'];
 				return moment((startTime.valueOf() + endTime.valueOf()) / 2)
 			}, type: 'datetime'},
-			{id: 'day', label: 'Day', derivativeFn: function (cleansedData, rawRow) {
+			{id: 'day', label: 'Day', derivativeFn: (cleansedData, rawRow) => {
 				return cleansedData['time'].clone().startOf('day');
 			}, type: 'date'},
 			{id: 'type', label: 'Feed Type', type: 'string'},
@@ -175,7 +175,7 @@ class Feeds extends Data {
 	get sessions () {
 		let fifteenMinutes = 15 * 60 * 1000;
 		let sessions = [];
-		_.each(this.data, function (feeding) {
+		_.each(this.data, (feeding) => {
 			if (sessions.length) {
 				let lastSession = _.last(sessions);
 				let timeBetween = Math.max(feeding.start.valueOf() - lastSession.end.valueOf(), lastSession.start.valueOf() - feeding.end.valueOf());
@@ -200,7 +200,7 @@ class Feeds extends Data {
 		dataTable.addColumn({id: 'median', label: 'Median Time Between Feedings', type: 'number'});
 		dataTable.addColumn({id: 'feedingsPerDay', label: 'Feedings Per Day', type: 'number'});
 
-		_.each(this.sessionsByDay, function (feedings, day) {
+		_.each(this.sessionsByDay, (feedings, day) => {
 			feedings = _.sortBy(feedings, 'start');
 			let durationsBetween = [];
 			for (let i=0; i < feedings.length - 1; i++) {
@@ -262,7 +262,7 @@ class Growths extends Data {
 		if (!this.data.length) {
 			return 0;
 		}
-		var sortedByDate = _.chain(this.data).filter(function (growth) {
+		var sortedByDate = _.chain(this.data).filter((growth) => {
 			return growth.weight > 0;
 		}).sortBy('day').value();
 		var latestWeight = this.convertToKg(_.last(sortedByDate).weight, _.last(sortedByDate).weightUnit);
@@ -277,7 +277,7 @@ class Growths extends Data {
 		dataTable.addColumn({id: 'p25', label: '25th Percentile', type: 'number', role: 'interval'});
 		dataTable.addColumn({id: 'p75', label: '75th Percentile', type: 'number', role: 'interval'});
 
-		_.each(this.data, function (growth) {
+		_.each(this.data, (growth) => {
 			if (growth.weight) {
 				let age = metadata.ageOnDate(growth.day);
 				let row = [growth.day.toDate(),
@@ -301,7 +301,7 @@ class Sleeps extends Data {
 		super(data, [
 			{id: 'id', label: 'id', type: 'number'},
 			{id: 'start', label: 'Start Time', type: 'datetime'},
-			{id: 'end', label: 'End Time', derivativeFn: function (cleansedData, rawRow) {
+			{id: 'end', label: 'End Time', derivativeFn: (cleansedData, rawRow) => {
 				let startTime = cleansedData['start'];
 				let endTime = rawRow['End Time'];
 				if (endTime) {
@@ -310,23 +310,23 @@ class Sleeps extends Data {
 					return startTime.clone().add(rawRow['Approximate Duration (Minutes)'], 'minutes');
 				}
 			}, type: 'datetime'},
-			{id: 'time', label: 'Time', derivativeFn: function (cleansedData, rawRow) {
+			{id: 'time', label: 'Time', derivativeFn: (cleansedData, rawRow) => {
 				let startTime = cleansedData['start'];
 				let endTime = cleansedData['end'];
 				return moment((startTime.valueOf() + endTime.valueOf()) / 2)
 			}, type: 'datetime'},
 			{id: 'note', label: 'Note', type: 'string'},
 			{id: 'duration', label: 'Duration', originalLabel: 'Approximate Duration (Minutes)', type: 'number'},
-			{id: 'type', label: 'Type', derivativeFn: function (cleansedData, rawRow) {
+			{id: 'type', label: 'Type', derivativeFn: (cleansedData, rawRow) => {
 				let time = cleansedData['start'];
 				return time.hours() >= dayNightStartHour|| time.hours() < dayNightEndHour ? 'Night' : 'Nap';
 			}, type: 'string'},
-			{id: 'day', label: 'Day', derivativeFn: function (cleansedData, rawRow) {
+			{id: 'day', label: 'Day', derivativeFn: (cleansedData, rawRow) => {
 				let time = cleansedData['time'];
 				let dayAdjustment = (time.hours() < dayNightEndHour) ? -1 : 0;
 				return time.clone().startOf('day').add(dayAdjustment, 'days');
 			}, type: 'date'},
-			{id: 'durationHour', label: 'Duration', derivativeFn: function (cleansedData, rawRow) {
+			{id: 'durationHour', label: 'Duration', derivativeFn: (cleansedData, rawRow) => {
 				return math.round(cleansedData['duration'] / 60, 1);
 			}, type: 'number'}
 		], dateFormat);
@@ -344,7 +344,7 @@ class Sleeps extends Data {
 			}
 			let longest = durations.splice(0, n);
 			// Add remainder
-			longest.push(_.reduce(durations, function (s, n) {
+			longest.push(_.reduce(durations, (s, n) => {
 				return s+n;
 			}, 0));
 			// Ensure we have n + 1 results
@@ -360,7 +360,7 @@ class Sleeps extends Data {
 		dataTable.addColumn({id: 'secondLongest', label: 'Second Longest', type: 'number'});
 		dataTable.addColumn({id: 'remainder', label: 'Remainder', type: 'number'});
 
-		_.each(this.sleepsByDay, function (sleepDay, day) {
+		_.each(this.sleepsByDay, (sleepDay, day) => {
 			let halfDaySleep = _.where(sleepDay, {'type': type});
 			let sleepDurations = longestDurations(halfDaySleep, 2);
 			sleepDurations.unshift(new Date(day));
@@ -386,7 +386,7 @@ class Sleeps extends Data {
 	}
 
 	get totalHours() {
-		return _.reduce(this.data, function (hours, sleep) {
+		return _.reduce(this.data, (hours, sleep) => {
 			return hours + sleep.duration / 60;
 		}, 0);
 	}
@@ -417,11 +417,11 @@ class Journals extends Data {
 
 class TimelineData {
 	constructor (datas, startDate, endDate) {
-		var timelineViews = _.map(datas, function (data, category) {
+		var timelineViews = _.map(datas, (data, category) => {
 			let timelineView = data.dataView([
-				{id: 'category', label: 'Category', type: 'string', calc: function () { return category; }},
+				{id: 'category', label: 'Category', type: 'string', calc: () => category},
 				'type',
-				{id: 'tooltip', label: 'Tooltip', type: 'string', role: 'tooltip', p: {html:true}, calc: function (datatable, row) {
+				{id: 'tooltip', label: 'Tooltip', type: 'string', role: 'tooltip', p: {html:true}, calc: (datatable, row) => {
 					let type = datatable.getValue(row, data.columnIdToIndex('type'));
 					let start = datatable.getValue(row, data.columnIdToIndex('start'));
 					let end = datatable.getValue(row, data.columnIdToIndex('end'));
@@ -445,7 +445,7 @@ class TimelineData {
 			return timelineView;
 		});
 
-		var combinedJson = _.reduce(timelineViews, function (combined, timelineView) {
+		var combinedJson = _.reduce(timelineViews, (combined, timelineView) => {
 			var jsonObject = JSON.parse(timelineView.toDataTable().toJSON());
 			if (combined.rows) {
 				jsonObject.rows = combined.rows.concat(jsonObject.rows);
